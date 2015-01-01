bc73d3d22a730873fd72fd6afe35d6b1cb703db3
hlukovac <hhaarree@hotmail.com>
2015-01-01 18:25:34 +0100

    Strings and Lists

---
+++ 003_Python_Village_INI3.py
@@ -0,0 +1,4 @@
+s = "gqYYPhwvhpNilYnJ3yG584k6wDFpG9SOVNYJO3exPDzpzbPhrynocephalusnQZV5fMhwynZOku8pabyfEMiHNcqnowr4g2J0aAnQt1l7MOkk20hKBK7cVCY3NM8ptXnioMZPFVuplxkKccompletusQHOlDSwum9."
+a, b, c, d = 46, 59, 142, 150
+
+print(s[a:b+1], s[c:d+1])
