f856d92b16fbecffb08b7f7473e6a2e77343b26e
hlukovac <hhaarree@hotmail.com>
2015-01-01 18:19:32 +0100

    Variables and Some Arithmetic

--- 002_Python_Village_INI2.py
+++ 002_Python_Village_INI2.py
@@ -1,10 +1,5 @@
-with open("Data/rosalind_ini2.txt") as input_data:
-	a, b = map(int, input_data.read().strip().split())
-	
-	
+a = 963
+b = 824
+c = (a*a)+(b*b)
 
-c = (a*a) + (b*b)
 print(c)
-
-with open("Output/001_INI2.txt", "w") as output_data:
-		output_data.write(str(c))
